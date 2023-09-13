e252063d1bfeb2ae10c86ea576cd04a4b1feeeb4
Ankit BagheL <ak9664773@gmail.com>
2023-09-13 19:42:10 +0530

    add header and search componnet

--- src/components/Header/Search/Search.jsx
+++ src/components/Header/Search/Search.jsx
@@ -1,6 +1,31 @@
+// import React, { useState } from "react";
+import { MdClose } from "react-icons/md";
 import "./Search.scss";
-const Search = () => {
-    return <div>Search</div>;
+import prod from "../../../assets/products/earbuds-prod-1.webp"
+
+// import useFetch from "../../../hooks/useFetch";
+// import { useNavigate } from "react-router-dom";
+
+const Search = ({ setShowSearch }) => {
+    return <div className="search-modal">
+        <div className="form-field">
+            <input type="text" autoFocus placeholder="Search for products" />
+            <MdClose className="close-btn" onClick={() => { setShowSearch(false) }} />
+        </div>
+        <div className="search-result-content">
+            <div className="search-results">
+                <div className="search-result-item">
+                    <div className="image-container">
+                        <img src={prod} alt="" />
+                    </div>
+                    <div className="prod-details">
+                        <div className="name">Product Name</div>
+                        <div className="desc">Product desc</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>;
 };
 
 export default Search;
