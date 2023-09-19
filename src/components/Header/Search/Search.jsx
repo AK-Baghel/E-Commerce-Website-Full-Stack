efce3e4f60f5ace154c101db4111a8e07feabc86
Ankit BagheL <ak9664773@gmail.com>
2023-09-19 18:59:58 +0530

    add search function in search component

--- src/components/Header/Search/Search.jsx
+++ src/components/Header/Search/Search.jsx
@@ -1,28 +1,53 @@
-// import React, { useState } from "react";
+import React, { useState } from "react";
 import { MdClose } from "react-icons/md";
 import "./Search.scss";
 import prod from "../../../assets/products/earbuds-prod-1.webp"
 
-// import useFetch from "../../../hooks/useFetch";
-// import { useNavigate } from "react-router-dom";
+import useFetch from "../../../hooks/useFetch";
+import { useNavigate } from "react-router-dom";
 
 const Search = ({ setShowSearch }) => {
+    const [query, setQuery] = useState("")
+    const navigate = useNavigate();
+
+    const onChange = (e) => {
+        setQuery(e.target.value);
+    }
+
+    let { data } = useFetch(`/api/products?populate=*&filters[title][$contains]=${query}`)
+    if (!query.length) {
+        data = null;
+    }
+
+
     return <div className="search-modal">
         <div className="form-field">
-            <input type="text" autoFocus placeholder="Search for products" />
+            <input type="text" autoFocus placeholder="Search for products" value={query} onChange={onChange} />
             <MdClose className="close-btn" onClick={() => { setShowSearch(false) }} />
         </div>
         <div className="search-result-content">
             <div className="search-results">
-                <div className="search-result-item">
-                    <div className="image-container">
-                        <img src={prod} alt="" />
-                    </div>
-                    <div className="prod-details">
-                        <div className="name">Product Name</div>
-                        <div className="desc">Product desc</div>
-                    </div>
-                </div>
+                {data &&
+                    data.data.map((item) => {
+                        return (
+                            <div key={item.id} className="search-result-item" onClick={()=>{
+                                navigate('/product/'+item.id)
+                                setShowSearch(false)
+                                }}>
+                                <div className="image-container">
+                                    <img src={
+                                        `http://localhost:1337${item.attributes.img.data[0].attributes.url}`
+                                    } alt="" />
+                                </div>
+                                <div className="prod-details">
+                                    <div className="name">{item.attributes.title}</div>
+                                    <div className="desc">{item.attributes.desc}</div>
+                                </div>
+                            </div>
+
+                        )
+                    })
+                }
             </div>
         </div>
     </div>;
