7d776e74a32bc61cf3c855763bdaaded5ea893f7
Ankit BagheL <ak9664773@gmail.com>
2023-09-21 20:11:51 +0530

    update favourite component

--- src/components/Favourite/Favourite.jsx
+++ src/components/Favourite/Favourite.jsx
@@ -1,26 +1,61 @@
-import React, { useContext, useState } from "react"
-import {Products} from "../Products/Products"
+import React, { useContext, useState, useEffect } from "react"
+import Product  from "../Products/Product/Product"
 import { Context } from "../../utils/context";
+import "./Favourite.scss"
 
 const Favourite = ({ setShowFav }) => {
     const { totalFav } = useContext(Context);
-    const { data, setData } = useState([]);
+    const [data, setData] = useState([]);
+
+    // const fetchData = async () => {
+    //     try {
+    //         const response = await fetch('http://localhost:1337/api/categories?populate=*');
+    //         const result = await response.json();
+    //         const fetchedData = result.data;
+    //         setData(fetchedData);
+    //         console.log(data);
+    //     } catch (error) {
+    //         console.error('Error fetching data:', error);
+    //     }
+    // }
+
     const fetchData = async () => {
-        let data = await fetch('http://localhost:1337/api/products')
-        let result = await data.json();
-        result=result.data;
-        console.log(result);
-        
-    }
-    fetchData();
+        try {
+          const response = await fetch('http://localhost:1337/api/categories?populate=*');
+          const result = await response.json();
+          const fetchedData = result.data;
+          setData(fetchedData);
+          console.log(fetchedData);
+        } catch (error) {
+          console.error('Error fetching data:', error);
+        }
+      }
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
     return (
-        <div className="category-main-content">
-            <div className="layout">
-                <div className="category-title">
+        <div className="category-fav-content">
+            <div className="fav-layout">
+                <div className="fav-title">
                     Favourites
                 </div>
-                {/* <Products innerPage={true} products={data} /> */}
+                <div className="close" onClick={() => { setShowFav(false) }}>X Close</div>
             </div>
+            <hr />
+            {
+                // data.map((item) => {
+                //     return (
+                //         <>
+                //             {/* <Product key={data.id} data={data}/> */}
+                            
+                //         </>
+
+                //     )
+                // })
+            }
+            {/* <Product id={6} data={7}  /> */}
         </div>
     )
 }
